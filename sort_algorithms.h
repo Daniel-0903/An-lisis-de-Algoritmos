1233ffc8c0a50b7ae82ac0ec1a620f3d53420c9b
Daniel-0903 <dgonzalezchaves@gmail.com>
2024-09-16 16:07:10 -0600

    bubble y selection

---
+++ sort_algorithms.h
@@ -0,0 +1,8 @@
+#ifndef SORT_ALGORITHMS_H
+#define SORT_ALGORITHMS_H
+
+void bubbleSort(int arr[], int n);
+void selectionSort(int arr[], int n);
+void mergeSort(int arr[], int l, int r);
+
+#endif // SORT_ALGORITHMS_H
